773331ee5068a3a7980c610266e51ad608ba8940
darrenkuro <odon5ht@gmail.com>
2025-12-19 07:46:33 +0100

    Improve style for ft_read

--- src/x86_64/ft_read.s
+++ src/x86_64/ft_read.s
@@ -6,8 +6,7 @@ ft_read:            ; rdi, rsi, rdx for parameters
 	mov		rax, 0  ; 0 - read syscall
 	syscall
 	cmp		rax, 0  ; rax - 0 (sign check)
-	js		.errno  ; check sign flag (SF) which is assigned by syscall
-                    ; if it's neg; in this case, jl works too
+	js		.errno  ; jump if signed (neg)
 	ret
 
 .errno:
@@ -15,6 +14,6 @@ ft_read:            ; rdi, rsi, rdx for parameters
 	push	rax				; push & pop are stack ops for temp data
                             ; do not use register, implicitly QWORD
 	call	__errno_location; external func to get errno addr to rax
-	pop		QWORD [rax]		; pop the QWORD (8 bytes, implied) to where rax points
+	pop		[rax]		    ; pop (rax pushed/errno) to where rax points
 	mov		rax, -1			; reset to -1
 	ret
