82bfdc16f02fdbc531e108d0879d86eff6fcb970
darrenkuro <odon5ht@gmail.com>
2025-12-19 12:15:46 +0100

    Improve ft_read and ft_write

--- src/x86_64/ft_read.s
+++ src/x86_64/ft_read.s
@@ -3,17 +3,17 @@ global  ft_read
 extern  __errno_location
 
 ft_read:                 ; rdi, rsi, rdx for parameters
-	mov rax, 0           ; 0 - read syscall
+	mov  eax, 0          ; 0 - read syscall
 	syscall
-	cmp rax, 0           ; rax - 0 (sign check)
-	js  .errno           ; jump if signed (neg)
+	test eax, eax        ; sign check
+	js   .errno          ; jump if signed (neg)
 	ret
 
 .errno:
-	neg  rax              ; get the positive errno, how it is defined
-	push rax              ; push & pop are stack ops for temp data
-	                      ; do not use register, implicitly QWORD
+	neg  eax              ; get the positive errno, how it is defined
+	mov  edi, eax         ; save errno in edi (knowing errno_location signature)
+	                      ; if using stack, need to ensure alignment
 	call __errno_location ; external func to get errno addr to rax
-	pop  QWORD [rax]      ; pop (rax pushed/errno) to where rax points
-	mov  rax, -1          ; reset to -1
+	mov  [rax], edi       ; restore errno to location, DWORD implictly due to edi
+	mov  rax, -1          ; set return value to -1
 	ret
--- src/x86_64/ft_write.s
+++ src/x86_64/ft_write.s
@@ -14,6 +14,6 @@ ft_write:                 ; rdi, rsi, rdx for parameters
 	mov  edi, eax         ; save errno in edi (knowing errno_location signature)
 	                      ; if using stack, need to ensure alignment
 	call __errno_location ; external func to get errno addr to rax
-	mov  [rax], edi       ; restore errno to location
+	mov  [rax], edi       ; restore errno to location, DWORD implictly due to edi
 	mov  rax, -1          ; set return value to -1
 	ret
