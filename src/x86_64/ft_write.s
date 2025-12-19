c0ee5a335d2ad678c123890fe7df86c14ae04ad3
darrenkuro <odon5ht@gmail.com>
2025-12-19 12:08:38 +0100

    Fix operation size

--- src/x86_64/ft_write.s
+++ src/x86_64/ft_write.s
@@ -14,6 +14,6 @@ ft_write:                 ; rdi, rsi, rdx for parameters
 	mov  edi, eax         ; save errno in edi (knowing errno_location signature)
 	                      ; if using stack, need to ensure alignment
 	call __errno_location ; external func to get errno addr to rax
-	mov  QWORD [rax], edi ; restore errno to location
+	mov  [rax], edi ; restore errno to location
 	mov  eax, -1          ; set return value to -1
 	ret
